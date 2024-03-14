19b565231b237a8768888a6bff0989f9074709e6
Pablo Sanchez <pablo.sanchez@itti.digital>
2024-03-14 00:00:58 -0300

    chore: test dev

--- legacy/staging/build_android.sh
+++ legacy/staging/build_android.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 echo "building staging android artifact..."
+echo "$ANDROID_STAGING_FIREBASE" > credentials/firebase/google-services.json
+flutter pub run flutter_flavorizr -p assets:download
+flutter pub run flutter_flavorizr -p assets:extract
+flutter pub run flutter_flavorizr -p ios:xcconfig
+flutter pub run flutter_flavorizr -p google:firebase
+flutter pub run flutter_flavorizr -p assets:clean
 make remove_huawei
 sh ./frontend-core-cicd/legacy/build.sh apk staging_firebase lib/main_staging_firebase.dart
