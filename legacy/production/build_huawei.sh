96444f6bd38970b61a33a56e0e87805ee6ad11cc
Pablo Sanchez <pablo.sanchez@itti.digital>
2024-03-14 13:14:16 -0300

    chore: test dev

--- legacy/production/build_huawei.sh
+++ legacy/production/build_huawei.sh
@@ -1,6 +1,6 @@
-
 echo "Executing Huawei build..."
+echo "$ANDROID_PRODUCTION_HUAWEI" > credentials/huawei/agconnect-services.json
 
-make remove_firebase
 flutter pub run flutter_flavorizr -p huawei:agconnect
+make remove_firebase
 sh ./frontend-core-cicd/legacy/build.sh appbundle production_huawei lib/main_production_huawei.dart
