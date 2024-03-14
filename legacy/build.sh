8a9211ce2d557b4d869a3bf8fb4d3fafae1e8ec2
Pablo Sanchez <pablo.sanchez@itti.digital>
2024-03-13 22:49:14 -0300

    chore: test dev

--- legacy/build.sh
+++ legacy/build.sh
@@ -3,4 +3,6 @@
 echo "building artifacts..."
 extra_build_args=${@:4}
 flutter version
-flutter build $1 --release --dart-define=UENO_ENVIRONMENT=$2 --dart-define=UENO_USE_MOCKS=false --dart-define=UENO_CRASHLYTICS_PROVIDER=$3 --flavor $2 -t lib/main.dart $extra_build_args
+flutter build $1 --release --flavor $2 -t $3 $extra_build_args
+
+# flutter build ipa --release --flavor production_firebase -t lib/main_production_firebase.dart --export-options-plist=/Users/builder/export_options.plist
---
+++ legacy/production/build_ios.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "building production ios artifact..."
+
+xcode-project use-profiles
+
+echo "$IOS_PRODUCTION_FIREBASE" > credentials/firebase/GoogleService-Info.plist
+flutter pub run flutter_flavorizr -p assets:download
+flutter pub run flutter_flavorizr -p assets:extract
+flutter pub run flutter_flavorizr -p ios:xcconfig
+flutter pub run flutter_flavorizr -p google:firebase
+flutter pub run flutter_flavorizr -p assets:clean
+make remove_huawei
+xcode-project use-profiles
+sh ./frontend-core-cicd/build.sh ipa production_firebase lib/main_production_firebase.dart --export-options-plist=/Users/builder/export_options.plist
+
+# flutter build ipa --release --flavor production_firebase -t lib/main_production_firebase.dart --export-options-plist=/Users/builder/export_options.plist
