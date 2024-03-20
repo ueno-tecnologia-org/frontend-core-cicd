ff8dbe75bce288d97b9bea209c6916f8a1804989
Pablo Sanchez <pablo.sanchez@itti.digital>
2024-03-20 20:37:00 -0300

    feat: chore test

--- legacy/production/build_android.sh
+++ legacy/production/build_android.sh
@@ -7,5 +7,5 @@ flutter pub run flutter_flavorizr -p assets:extract
 flutter pub run flutter_flavorizr -p ios:xcconfig
 flutter pub run flutter_flavorizr -p google:firebase
 flutter pub run flutter_flavorizr -p assets:clean
-make remove_huawei
+# make remove_huawei
 sh ./frontend-core-cicd/legacy/build.sh apk production_firebase lib/main_production_firebase.dart
