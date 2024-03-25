642d685484aa3399a008cb22c2a833a4a554b1dd
Pablo Sanchez <pablo.sanchez@itti.digital>
2024-03-25 09:55:09 -0300

    feat: uncommnet make remove huawei and firebase

--- legacy/development/build_android.sh
+++ legacy/development/build_android.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "building development android artifact..."
-# make remove_huawei
+make remove_huawei
 sh ./frontend-core-cicd/legacy/build.sh apk development_firebase lib/main_development_firebase.dart
--- legacy/staging/build_android.sh
+++ legacy/staging/build_android.sh
@@ -8,5 +8,5 @@ flutter pub run flutter_flavorizr -p assets:extract
 flutter pub run flutter_flavorizr -p ios:xcconfig
 flutter pub run flutter_flavorizr -p google:firebase
 flutter pub run flutter_flavorizr -p assets:clean
-# make remove_huawei
+make remove_huawei
 sh ./frontend-core-cicd/legacy/build.sh apk staging_firebase lib/main_staging_firebase.dart
