3259376c43b3517dca508bd87a157fad733c8e93
Pablo Sanchez <pablo.sanchez@itti.digital>
2024-03-25 10:16:01 -0300

    feat: uncommnet make remove huawei and firebase

--- legacy/development/build_ios.sh
+++ legacy/development/build_ios.sh
@@ -2,6 +2,5 @@
 
 echo "building development ios artifact..."
 
-make remove_huawei
 xcode-project use-profiles
 sh ./frontend-core-cicd/legacy/build.sh ipa development_firebase lib/main_development_firebase.dart --export-options-plist=/Users/builder/export_options.plist
